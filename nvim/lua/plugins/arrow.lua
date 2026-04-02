f508bbd738191f82c4c010d99410dfcb9a88d020
crrsh <61464557+crrsh@users.noreply.github.com>
2026-04-02 21:18:41 +0100

    add arrow.nvim

---
+++ nvim/lua/plugins/arrow.lua
@@ -0,0 +1,10 @@
+return {
+	"otavioschwanck/arrow.nvim",
+	dependencies = { "echasnovski/mini.icons" },
+	opts = {
+		leader_key = ";",
+		buffer_leader_key = "m",
+		hide_handbook = true,
+		hide_buffer_handbook = true,
+	},
+}
