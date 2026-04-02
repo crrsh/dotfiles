436f3dbf5be153671a7eb5b0dfdb57f269836d1f
crrsh <61464557+crrsh@users.noreply.github.com>
2026-04-02 20:08:07 +0100

    update mini.ai config - updated textobjects to match nvim-treesitter-textobjects - h and l not currently working how I envisioned

--- nvim/lua/plugins/mini-ai.lua
+++ nvim/lua/plugins/mini-ai.lua
@@ -1,28 +1,50 @@
 return {
 	"nvim-mini/mini.ai",
-	version = false,
 	event = "VeryLazy",
-	keys = { { "cic", "cic<space>", remap = true } },
 	opts = function()
 		local ai = require("mini.ai")
 		return {
 			n_lines = 500,
+			mappings = {
+				-- disable because not likely to use
+				around_next = "",
+				inside_next = "",
+				around_last = "",
+				inside_last = "",
+			},
 			custom_textobjects = {
 				o = ai.gen_spec.treesitter({ -- code block
 					a = { "@block.outer", "@conditional.outer", "@loop.outer" },
 					i = { "@block.inner", "@conditional.inner", "@loop.inner" },
 				}),
-				f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
-				C = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
-				c = ai.gen_spec.treesitter({ a = "@comment.outer", i = "@comment.inner" }), -- comment
-				t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
+				m = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+				c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
+				C = ai.gen_spec.treesitter({ a = "@comment.outer", i = "@comment.inner" }), -- comment
+
 				d = { "%f[%d]%d+" }, -- digits
-				e = { -- Word with case
+				e = { -- word with case
 					{ "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
 					"^().*()$",
 				},
-				u = ai.gen_spec.function_call(), -- u for "Usage"
-				U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
+				g = function() -- entire buffer
+					local from = { line = 1, col = 1 }
+					local to = {
+						line = vim.fn.line("$"),
+						col = math.max(vim.fn.getline("$"):len(), 1),
+					}
+					return { from = from, to = to }
+				end,
+
+				-- assignment
+				-- TODO: doesn't really behave as I want
+				h = ai.gen_spec.treesitter({
+					a = { "@assignment.lhs", "@pair.key" },
+					i = { "@assignment.lhs", "@pair.key" },
+				}),
+				l = ai.gen_spec.treesitter({
+					a = { "@assignment.rhs", "@pair.value" },
+					i = { "@assignment.rhs", "@pair.value" },
+				}),
 			},
 		}
 	end,
