551b908d77645e683203563c5750f6b28d708ab8
crrsh <61464557+crrsh@users.noreply.github.com>
2026-04-02 19:39:05 +0100

    add back nvim-treesitter-textobjects - added a bunch of move keymaps that I may never use

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -49,4 +49,77 @@ return {
 			})
 		end,
 	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		event = "VeryLazy",
+		init = function()
+			-- disable entire built-in ftplugin mappings to avoid conflicts.
+			vim.g.no_plugin_maps = true
+		end,
+		config = function()
+			require("nvim-treesitter-textobjects").setup({
+				select = { lookahead = true },
+				move = { set_jumps = true },
+			})
+
+			local move = require("nvim-treesitter-textobjects.move")
+			local swap = require("nvim-treesitter-textobjects.swap")
+
+			-- swap
+			-- @parameter.inner
+			vim.keymap.set("n", "<M-l>", function()
+				swap.swap_next("@parameter.inner")
+			end)
+			vim.keymap.set("n", "<M-h>", function()
+				swap.swap_previous("@parameter.inner")
+			end)
+
+			-- move
+			-- @call.outer
+			vim.keymap.set({ "n", "x", "o" }, "]f", function()
+				move.goto_next_start("@call.outer", "textobjects")
+			end)
+			vim.keymap.set({ "n", "x", "o" }, "]F", function()
+				move.goto_next_end("@call.outer", "textobjects")
+			end)
+			vim.keymap.set({ "n", "x", "o" }, "[f", function()
+				move.goto_previous_start("@call.outer", "textobjects")
+			end)
+			vim.keymap.set({ "n", "x", "o" }, "[F", function()
+				move.goto_previous_end("@call.outer", "textobjects")
+			end)
+
+			-- @function.outer
+			vim.keymap.set({ "n", "x", "o" }, "]m", function()
+				move.goto_next_start("@function.outer", "textobjects")
+			end)
+			vim.keymap.set({ "n", "x", "o" }, "]M", function()
+				move.goto_next_end("@function.outer", "textobjects")
+			end)
+			vim.keymap.set({ "n", "x", "o" }, "[m", function()
+				move.goto_previous_start("@function.outer", "textobjects")
+			end)
+			vim.keymap.set({ "n", "x", "o" }, "[M", function()
+				move.goto_previous_end("@function.outer", "textobjects")
+			end)
+
+			-- @loop.inner, @loop.outer
+			vim.keymap.set({ "n", "x", "o" }, "]o", function()
+				move.goto_next_start({ "@loop.inner", "@loop.outer" }, "textobjects")
+			end)
+
+			-- @condition.outer
+			vim.keymap.set({ "n", "x", "o" }, "]d", function()
+				move.goto_next("@conditional.outer", "textobjects")
+			end)
+			vim.keymap.set({ "n", "x", "o" }, "[d", function()
+				move.goto_previous("@conditional.outer", "textobjects")
+			end)
+
+			-- @local.scope
+			vim.keymap.set({ "n", "x", "o" }, "]s", function()
+				move.goto_next_start("@local.scope", "locals")
+			end)
+		end,
+	},
 }
