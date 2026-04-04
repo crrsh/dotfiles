c4e026c23dde0db024a06dc86a6f06f2c1e29498
crrsh <61464557+crrsh@users.noreply.github.com>
2026-04-04 23:39:28 +0100

    make completion menu prettier - add snippets source with `friendly-snippets` - add `Ctrl-s` keymap to show completion menu with only snippets - hide snippets by default from completion menu - use `colorful-menu.nvim` to get context aware ts highlighting in   completion menu - use `mini.icons` to get highlighted filetype icons (prefer regular   kind icons for everything else)

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -1,5 +1,24 @@
+local function get_mini_file_icon(ctx)
+	if ctx.source_name == "Path" then
+		local is_unknown_type =
+			vim.tbl_contains({ "link", "socket", "fifo", "char", "block", "unknown" }, ctx.item.data.type)
+		local mini_icon, mini_hl, _ = require("mini.icons").get(
+			is_unknown_type and "os" or ctx.item.data.type,
+			is_unknown_type and "" or ctx.label
+		)
+		if mini_icon then
+			return mini_icon or nil, mini_hl
+		end
+	end
+end
+
 return {
 	"saghen/blink.cmp",
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+		"xzbdmw/colorful-menu.nvim",
+		"nvim-mini/mini.icons",
+	},
 	event = { "InsertEnter", "CmdlineEnter" },
 	version = "1.*",
 
@@ -7,17 +26,39 @@ return {
 	---@type blink.cmp.Config
 	opts = {
 		sources = {
-			default = { "lsp", "path", "snippets" },
+			default = { "lsp", "path" },
 		},
 		completion = {
 			menu = {
 				max_height = 8,
-				draw = { treesitter = { "lsp" } },
+				draw = {
+					columns = { { "kind_icon" }, { "label", gap = 1 } },
+					components = {
+						kind_icon = {
+							text = function(ctx)
+								local mini_icon, _ = get_mini_file_icon(ctx)
+								return mini_icon or ctx.kind_icon
+							end,
+							highlight = function(ctx)
+								local _, mini_hl = get_mini_file_icon(ctx)
+								return mini_hl or require("colorful-menu").blink_components_highlight(ctx)
+							end,
+						},
+						label = {
+							text = function(ctx)
+								return require("colorful-menu").blink_components_text(ctx)
+							end,
+							highlight = function(ctx)
+								return require("colorful-menu").blink_components_highlight(ctx)
+							end,
+						},
+					},
+				},
 			},
 		},
 		signature = {
 			enabled = true,
-			trigger = { enabled = false },
+			trigger = { enabled = false }, -- trigger manually with Ctrl-k
 			window = {
 				show_documentation = false,
 				direction_priority = { "n", "s" }, -- keep out of the way of completion menu
@@ -26,6 +67,11 @@ return {
 		keymap = {
 			["<C-u>"] = { "scroll_documentation_up", "fallback" },
 			["<C-d>"] = { "scroll_documentation_down", "fallback" },
+			["<C-s>"] = {
+				function(cmp)
+					cmp.show({ providers = { "snippets" } })
+				end,
+			},
 		},
 	},
 	opts_extend = { "sources.default" },
