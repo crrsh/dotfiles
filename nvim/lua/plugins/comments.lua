54f808d180bd0e4573c084dd080a03a18398835a
crrsh <61464557+crrsh@users.noreply.github.com>
2026-04-05 17:38:31 +0100

    updated comments plugins configs - added keymap to open todo comments in trouble

--- nvim/lua/plugins/comments.lua
+++ nvim/lua/plugins/comments.lua
@@ -2,34 +2,22 @@ return {
 	{
 		"numToStr/Comment.nvim",
 		event = "VeryLazy",
-		enabled = false,
 		opts = {},
 	},
 	{
 		"folke/ts-comments.nvim",
 		event = "VeryLazy",
-		enabled = false,
 		opts = {},
 	},
 	{
 		"folke/todo-comments.nvim",
+		dependencies = { "folke/trouble.nvim" },
 		event = "VeryLazy",
-		enabled = false,
+    -- stylua: ignore
 		keys = {
-			{
-				"[t",
-				function()
-					require("todo-comments").jump_prev()
-				end,
-				desc = "Previous todo comment",
-			},
-			{
-				"]t",
-				function()
-					require("todo-comments").jump_next()
-				end,
-				desc = "Next todo comment",
-			},
+      { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+			{ "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+      { "<leader>xt", "<cmd>:Trouble todo toggle<cr>", desc = "Trouble todo" },
 		},
 		opts = {
 			signs = false,
