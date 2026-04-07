40d9df6092f990c3efd822da5dc07f074ffb516f
crrsh <61464557+crrsh@users.noreply.github.com>
2026-04-07 16:38:58 +0100

    workspace diagnostics on trouble diagnostics
    Neovim added native support for workspace diagnostics, but trouble does
    not use it yet https://github.com/folke/trouble.nvim/issues/682.

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,16 +1,13 @@
 return {
 	"folke/trouble.nvim",
 	cmd = "Trouble",
+  --stylua: ignore
 	keys = {
-		{ "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
-
+    -- TODO: update once fixed https://github.com/folke/trouble.nvim/issues/682
+    { "<leader>xx", function() if not require("trouble").is_open("diagnostics") then vim.lsp.buf.workspace_diagnostics() end vim.cmd("Trouble diagnostics toggle") end, desc = "Diagnostics (Trouble)" },
 		{ "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
 		{ "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)" },
-		{
-			"<leader>cl",
-			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-			desc = "LSP Definitions / references / ... (Trouble)",
-		},
+		{ "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP Definitions / references / ... (Trouble)" },
 		{ "<leader>xl", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
 		{ "<leader>xq", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
 	},
