4870e53401c3fb02303cf13e9bfa856e55986d2e
crrsh <61464557+crrsh@users.noreply.github.com>
2026-04-11 18:52:41 +0100

    re-enable flash

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,54 +1,14 @@
 return {
 	"folke/flash.nvim",
-	enabled = false,
-	event = "VeryLazy",
-	keys = {
-		{
-			"s",
-			mode = { "n", "x", "o" },
-			function()
-				require("flash").jump()
-			end,
-			desc = "Flash",
-		},
-		{
-			"S",
-			mode = { "n", "x", "o" },
-			function()
-				require("flash").treesitter()
-			end,
-			desc = "Flash Treesitter",
-		},
-		{
-			"r",
-			mode = "o",
-			function()
-				require("flash").remote()
-			end,
-			desc = "Remote Flash",
-		},
-		{
-			"R",
-			mode = { "o", "x" },
-			function()
-				require("flash").treesitter_search()
-			end,
-			desc = "Treesitter Search",
-		},
-		{
-			"<leader>s",
-			function()
-				require("flash").jump({ pattern = vim.fn.expand("<cword>") })
-			end,
-			desc = "Flash cword",
-		},
+	-- stylua: ignore
+  keys = {
+		{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+		{ "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+		{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+		{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+		{ "<leader>s", function() require("flash").jump({ pattern = vim.fn.expand("<cword>") }) end, desc = "Flash cword" },
 	},
 	opts = {
-		label = {
-			current = false,
-			after = false,
-			before = true, -- trying label before - won't move as you type
-		},
 		highlight = {
 			groups = {
 				-- TODO: make sure it looks nice in all themes - see :h flash
