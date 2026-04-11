a229c8e781df24264d1bc0c378f0db89ce865470
crrsh <61464557+crrsh@users.noreply.github.com>
2026-04-11 18:48:27 +0100

    fix nvim-surround
    v3->v4 changes how keymaps are set. Rather than bother I'm just going to
    run with the defaults it sets.

--- nvim/lua/plugins/nvim-surround.lua
+++ nvim/lua/plugins/nvim-surround.lua
@@ -1,18 +1,5 @@
 return {
 	"kylechui/nvim-surround",
 	event = "VeryLazy",
-	enabled = false,
-	opts = {
-		keymaps = { -- TODO: keymaps yet to be confirmed - conflicting with flash.nvim
-			-- i would be fine with defaults if i can settle on a different keymap for flash jump()
-			--                 -- like <cr> or <tab> or <ctrl-space> ???
-			normal = "gs", -- go surround
-			normal_cur = "gss",
-			normal_cur_lines = "gSS", -- this one does not work
-			visual = "gs",
-			visual_line = "gS",
-			delete = "dis", -- delete surround
-			change = "cis", -- change surround
-		},
-	},
+	opts = {},
 }
