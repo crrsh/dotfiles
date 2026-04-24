3dabb2cf9ba02919dd6991357289abee44ebf0e8
crrsh <61464557+crrsh@users.noreply.github.com>
2026-04-24 23:39:13 +0100

    disable diagnostics virtual text
    Also remove keymaps that are already defaults.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -40,7 +40,6 @@ return {
 		end,
 		config = function()
 			vim.diagnostic.config({
-				virtual_text = { current_line = true },
 				update_in_insert = true,
 				severity_sort = true,
 				signs = {
