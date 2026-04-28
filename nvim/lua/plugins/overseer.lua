bde39a7c2cfb84daa277a59dc2c3bdddf4480095
crrsh <61464557+crrsh@users.noreply.github.com>
2026-04-28 19:00:50 +0100

    overseer task list keymaps
    - disable `<c-j>` `<c-k>` for scrolling preview since I use these for
      jumping between windows
    - add `J` `K` for scrolling previews since otherwise unused in task list
      buffer
    - add keymaps to quickly restart/retain/watch/unwatch/ensure tasks
      rather than needing to pick the target task first in :OverseerTaskAction

--- nvim/lua/plugins/overseer.lua
+++ nvim/lua/plugins/overseer.lua
@@ -9,5 +9,21 @@ return {
 	},
 	---@module 'overseer'
 	---@type overseer.SetupOpts
-	opts = {},
+	opts = {
+		task_list = {
+			keymaps = {
+				-- change keys for scrolling preview since I use the defaults for jumping between windows
+				["<C-k>"] = false,
+				["<C-j>"] = false,
+				["K"] = "keymap.scroll_output_up",
+				["J"] = "keymap.scroll_output_down",
+				-- quicker actions
+				["r"] = { "keymap.run_action", opts = { action = "restart" }, desc = "Restart task" },
+				["R"] = { "keymap.run_action", opts = { action = "retain" }, desc = "Retain task" },
+				["w"] = { "keymap.run_action", opts = { action = "watch" }, desc = "Watch task" },
+				["u"] = { "keymap.run_action", opts = { action = "unwatch" }, desc = "Unwatch task" },
+				["e"] = { "keymap.run_action", opts = { action = "ensure" }, desc = "Ensure task" },
+			},
+		},
+	},
 }
