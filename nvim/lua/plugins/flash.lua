0a6d3ba102d304b033e3826ce967d3771f2002b5
crrsh <61464557+crrsh@users.noreply.github.com>
2026-07-20 22:44:30 +0100

    disable flash treesitter in visual mode
    S and vS already does exactly the same so no need for it, plus it
    conflicts with nvim-surround in visual mode.

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -3,7 +3,7 @@ return {
 	-- stylua: ignore
   keys = {
 		{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-		{ "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+		{ "S", mode = { "n", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
 		{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
 		{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
 		{ "<leader>s", function() require("flash").jump({ pattern = vim.fn.expand("<cword>") }) end, desc = "Flash cword" },
