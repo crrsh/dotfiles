7697bda13f71c9debdb03beca66369dca86ccb1f
crrsh <61464557+crrsh@users.noreply.github.com>
2026-08-15 14:49:40 +0100

    updated picker keymaps
    Mainly:
    - <leader>/ -> picker.lines()
    - <leader>. -> picker.resume()
    - marking duplicates with

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -5,43 +5,29 @@ return {
 	---@type snacks.Config
   -- stylua: ignore
 	keys = {
-		-- Top Pickers & Explorer
 		{ "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
-		{ "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
-		{ "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
-		{ "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
-		{ "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
+		{ "<leader>/", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
+		{ "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" }, -- TODO: duplicate
+		{ "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" }, -- TODO: duplicate
+		{ "<leader>.", function() Snacks.picker.resume() end, desc = "Resume" },
 		{ "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
-		-- find
-		{ "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
-		{ "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
+    -- find
 		{ "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+		{ "<leader>fF", function() Snacks.picker.files({hidden=true, ignored=true}) end, desc = "Find Files (include hidden/ignored)" },
+		{ "<leader>fg", function() Snacks.picker.grep() end, desc = "Grep" }, -- TODO: duplicate
+		{ "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" }, -- TODO: duplicate
+		{ "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
 		{ "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
-		{ "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
 		{ "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
-		-- git
-		{ "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
-		{ "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
-		{ "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
-		{ "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
-		{ "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
-		{ "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
-		{ "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
-		-- gh
-		{ "<leader>gi", function() Snacks.picker.gh_issue() end, desc = "GitHub Issues (open)" },
-		{ "<leader>gI", function() Snacks.picker.gh_issue({ state = "all" }) end, desc = "GitHub Issues (all)" },
-		{ "<leader>gp", function() Snacks.picker.gh_pr() end, desc = "GitHub Pull Requests (open)" },
-		{ "<leader>gP", function() Snacks.picker.gh_pr({ state = "all" }) end, desc = "GitHub Pull Requests (all)" },
-		-- Grep
-		{ "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
-		{ "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
+    -- search
+		{ "<leader>ss", function() Snacks.picker.grep() end, desc = "Grep" }, -- TODO: duplicate
+		{ "<leader>sg", function() Snacks.picker.grep() end, desc = "Grep" }, -- TODO: duplicate
+		{ "<leader>sb", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
 		{ "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
-		-- search
 		{ '<leader>s"', function() Snacks.picker.registers() end, desc = "Registers" },
 		{ "<leader>s/", function() Snacks.picker.search_history() end, desc = "Search History" },
 		{ "<leader>sa", function() Snacks.picker.autocmds() end, desc = "Autocmds" },
-		{ "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
-		{ "<leader>sc", function() Snacks.picker.command_history() end, desc = "Command History" },
+		{ "<leader>sc", function() Snacks.picker.command_history() end, desc = "Command History" }, -- TODO: duplicate
 		{ "<leader>sC", function() Snacks.picker.commands() end, desc = "Commands" },
 		{ "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
 		{ "<leader>sD", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
@@ -53,10 +39,23 @@ return {
 		{ "<leader>sl", function() Snacks.picker.loclist() end, desc = "Location List" },
 		{ "<leader>sm", function() Snacks.picker.marks() end, desc = "Marks" },
 		{ "<leader>sM", function() Snacks.picker.man() end, desc = "Man Pages" },
-		{ "<leader>sq", function() Snacks.picker.qflist() end, desc = "Quickfix List" },
-		{ "<leader>sR", function() Snacks.picker.resume() end, desc = "Resume" },
 		{ "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History" },
-		{ "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
+    -- git
+		{ "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
+		{ "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
+		{ "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
+		{ "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
+		{ "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
+		{ "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" }, 
+		{ "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" }, 
+    -- gh
+		{ "<leader>gi", function() Snacks.picker.gh_issue() end, desc = "GitHub Issues (open)" },
+		{ "<leader>gI", function() Snacks.picker.gh_issue({ state = "all" }) end, desc = "GitHub Issues (all)" },
+		{ "<leader>gp", function() Snacks.picker.gh_pr() end, desc = "GitHub Pull Requests (open)" },
+		{ "<leader>gP", function() Snacks.picker.gh_pr({ state = "all" }) end, desc = "GitHub Pull Requests (all)" },
+		{ "<leader>ga", function() Snacks.picker.gh_actions() end, desc = "GitHub actions (all)" },
+    -- ui
+		{ "<leader>uc", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
 		-- LSP
     -- TODO: try these out before adding
 		-- { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
