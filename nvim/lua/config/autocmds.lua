f76e897dedd738e33ec60633ba1797919d34a4ca
crrsh <61464557+crrsh@users.noreply.github.com>
2026-09-01 22:53:06 +0100

    Handy term autocmds

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,5 +1,35 @@
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, { -- add "CursorHold" to be more aggressive
-  desc = "Keep buffer contents updated",
-  command = "if mode() != 'c' | checktime | endif",
-  pattern = "*",
+	group = vim.api.nvim_create_augroup("autoreload", { clear = true }),
+	desc = "Keep buffer contents updated",
+	command = "if mode() != 'c' | checktime | endif",
+})
+
+local term = vim.api.nvim_create_augroup("term", { clear = true })
+vim.api.nvim_create_autocmd("WinLeave", {
+	group = term,
+	desc = "Remember terminal mode",
+	pattern = "term://*",
+	callback = function()
+		vim.b.term_mode = vim.api.nvim_get_mode().mode == "t"
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	group = term,
+	desc = "Restore terminal mode",
+	pattern = "term://*",
+	callback = function()
+		if vim.b.term_mode then
+			vim.cmd.startinsert()
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd("TermOpen", {
+	group = term,
+	desc = "Open terminal in insert mode",
+	callback = function()
+		vim.b.term_mode = true
+		vim.cmd.startinsert()
+	end,
 })
